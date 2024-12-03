1d8a831a991f407f02ccff1e172685d8561e2038
Hussain-Asim <stuhussain@gmail.com>
2024-12-03 21:47:21 +0500

    replace .innerHTML to textContent for reliable.

--- calc.js
+++ calc.js
@@ -3,9 +3,9 @@
 // multiply
 // divide   
 
-let num1 ;
+let num1;
 let operator;
-let num2 ;
+let num2;
 
 function sum(num1, num2) {
     return num1 + num2;
@@ -40,74 +40,127 @@ function operate(operator, num1, num2) {
 // console.log(operate("-",8, 3));
 
 
-// let display = document.querySelector(".display");
-// const digit9 = document.querySelector(".a9");
-// const digit8 = document.querySelector(".a8");
-// const digit7 = document.querySelector(".a7");
-// const digit6 = document.querySelector(".a6");
-// const digit5 = document.querySelector(".a5");
-// const digit4 = document.querySelector(".a4");
-// const digit3 = document.querySelector(".a3");
-// const digit2 = document.querySelector(".a2");
-// const digit1 = document.querySelector(".a1");
-// const digit0 = document.querySelector(".a0");
+let display = document.querySelector(".display");
+const digit9 = document.querySelector(".a9");
+const digit8 = document.querySelector(".a8");
+const digit7 = document.querySelector(".a7");
+const digit6 = document.querySelector(".a6");
+const digit5 = document.querySelector(".a5");
+const digit4 = document.querySelector(".a4");
+const digit3 = document.querySelector(".a3");
+const digit2 = document.querySelector(".a2");
+const digit1 = document.querySelector(".a1");
+const digit0 = document.querySelector(".a0");
+
+// if any button clicked of numbers then replace that box over 0
 
 
-// digit9.addEventListener("click", () => {
-//     display.innerHTML=digit9.innerText;
-// });
+digit9.addEventListener("click", () => {
+    // display.textContent+=digit9.textContent;
 
-// digit8.addEventListener("click", () => {
-//     display.innerHTML=digit8.innerText;
-// });
+        if (display.textContent == 0) {
+            display.textContent = digit9.textContent;
+        }
+        else {
+            display.textContent += digit9.textContent;
+        }
 
-// digit7.addEventListener("click", () => {
-//     display.innerHTML=digit7.innerText;
-// });
+});
 
-// digit6.addEventListener("click", () => {
-//     display.innerHTML=digit6.innerText;
-// });
+digit8.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit8.textContent;
+    }
+    else {
+        display.textContent += digit8.textContent;
+    }
+});
 
-// digit5.addEventListener("click", () => {
-//     display.innerHTML=digit5.innerText;
-// });
+digit7.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit7.textContent;
+    }
+    else {
+        display.textContent += digit7.textContent;
+    }
+});
 
-// digit4.addEventListener("click", () => {
-//     display.innerHTML=digit4.innerText;
-// });
+digit6.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit6.textContent;
+    }
+    else {
+        display.textContent += digit6.textContent;
+    }
+});
 
-// digit3.addEventListener("click", () => {
-//     display.innerHTML=digit3.innerText;
-// });
+digit5.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit5.textContent;
+    }
+    else {
+        display.textContent += digit5.textContent;
+    }
+});
 
-// digit2.addEventListener("click", () => {
-//     display.innerHTML=digit2.innerText;
-// });
-// let displayContentSave;
-// digit1.addEventListener("click", () => {
-//     display.innerHTML=digit1.innerText;
-//     displayContentSave = display.innerHTML;
-// });
+digit4.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit4.textContent;
+    }
+    else {
+        display.textContent += digit4.textContent;
+    }
+});
 
-// digit0.addEventListener("click", () => {
-//     display.innerHTML=digit0.innerText;
-//     displayContentSave = display.innerHTML;
+digit3.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit3.textContent;
+    }
+    else {
+        display.textContent += digit3.textContent;
+    }
+});
 
-// });
-// console.log(displayContentSave);
+digit2.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit2.textContent;
+    }
+    else {
+        display.textContent += digit2.textContent;
+    }
+});
+
+digit1.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit1.textContent;
+    }
+    else {
+        display.textContent += digit1.textContent;
+    }
+});
+
+digit0.addEventListener("click", () => {
+    if (display.textContent == 0) {
+        display.textContent = digit0.textContent;
+    }
+    else {
+        display.textContent += digit0.textContent;
+    }
+});
 
 // You should store the content of the display (the number) in a variable for use in the next step.
 
+//When empty shows 0 otherwise when clicked replace.
 // user input n + n (=press) value shown 
 // calculator should shown input value or after result at a time.
 // user input now n + n - n and so on , first pair value solve show when next operator (- pressed) will input. 
+
+
 // any operator will be less light in css so u know which operator is selected.
 // Round the answers with long decimals for not overflowing display.
 // Pressing = before entering operator or number cause nothing.
 // Pressing Clear remove all previous shits.
 // Divide n by 0 shows Error in dislay. 
-//
 // Disable the . button if there’s already a decimal separator in the display. 
 // Add X remove button so user can remove last n.
 // Add keyboard support to digits, operators, . , clear .....etc
