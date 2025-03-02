3ce7f1dde946e7791c9266c359377174f6f05aac
Hussain-Asim <stuhussain@gmail.com>
2025-03-02 17:32:38 +0500

    not sure about 3 vars, experimeting till it works and add operate function.

--- calc.js
+++ calc.js
@@ -1,19 +1,34 @@
-function add(a,b) {
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 }
 // console.log(divide(10,2));
 
-const a = "";
-const operator = "";
-const b = "";
+let a;
+let operator = "";
+let b;
 
-    
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+
+        case "-":
+            return subtract(a, b);
+
+        case "*":
+            return multiply(a, b);
+
+        case "/":
+            return divide(a, b);
+    }
+}
+console.log(operate("+", 1, 2));
